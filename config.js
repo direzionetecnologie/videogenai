e62785bfa3c00f31d3a76862c6bc13c093452760
direzionetecnologie <valentinasag2@gmail.com>
2025-11-03 19:18:21 +0100

    Refactor video section titles and contents

--- config.js
+++ config.js
@@ -3,30 +3,48 @@ window.CONFIG = {
   FORM_ENDPOINT: 'https://formsubmit.co/ajax/tech.transformation.mdst@gmail.com',
   VIDEO_BASE_URL: 'https://aiworkshopmediaset.s3.eu-west-1.amazonaws.com/video-genai/',
   sections: [
-    {
-      title: 'Reply Bitmama',
-      contents: [
         {
-          title: 'Ricostruzioni',
+          title: 'Reply Bitmama',
           videos: [
             { title: 'La strage di Erba', file: 'Reply_Scena_Strage_Di_Erba.mov' }
           ]
-        }
-      ]
-    },
-    {
-      title: 'Bad Idea',
-      contents: [
+        },
         {
-          title: 'La strage di Erba',
+          title: 'Kortocircuito',
           videos: [
-            { title: 'La strage di Erba', file: 'BadIdea_01 Scena Strage di Erba.mov' }
+            { title: 'La strage di Erba', file: 'Kortocircuito_ERBA FINAL 4K.mp4' },
+            { title: "L'omicidio di Laura Ziliani", file: 'Kortocircuito_edit_Test_Laura Ziliani.mp4' }
           ]
         },
         {
-          title: "L'omicidio di Laura Ziliani",
+          title: 'Jakala',
           videos: [
+            { title: 'La strage di Erba', file: 'Jakala_Strage_Erba_Output.mp4' }
+          ]
+        },
+        {
+          title: 'Accenture Song',
+          videos: [
+            { title: 'La strage di Erba', file: 'ACN_Song_Mediaset_Rosa e Olindo.mp4' }
+          ]
+        },
+        {
+          title: 'Autori Multimediali',
+          videos: [
+            { title: 'La strage di Erba', file: 'Autori-Multimediali_SEQUENZA-ERBA-demo.mp4' }
+          ]
+        },
+        {
+          title: 'Bad Idea',
+          videos: [
+            { title: 'La strage di Erba', file: 'BadIdea_01 Scena Strage di Erba.mov' },
             { title: "L'omicidio di Laura Ziliani", file: 'BadIdea_02 Scena Omicidio Laura Ziliani.mp4' }
+          ]
+        },
+        {
+          title: 'Fiven AI',
+          videos: [
+            { title: 'La strage di Erba', file: 'Fiven_AI_Video_Demo_Strage_Erba.mp4' }
           ]
         }
       ]
