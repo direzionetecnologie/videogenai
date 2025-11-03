624a1187bd56ed934ee2b0449d0e6f4824585216
direzionetecnologie <valentinasag2@gmail.com>
2025-11-03 19:05:01 +0100

    Fix malformed sections structure in config

--- config.js
+++ config.js
@@ -3,21 +3,18 @@ window.CONFIG = {
   FORM_ENDPOINT: 'https://formsubmit.co/ajax/tech.transformation.mdst@gmail.com',
   VIDEO_BASE_URL: 'https://aiworkshopmediaset.s3.eu-west-1.amazonaws.com/video-genai/',
   sections: [
-        {
-          title: 'Reply Bitmama',
-          videos: [
-            { title: 'La strage di Erba', file: 'Reply_Scena_Strage_Di_Erba.mov' }
-          ]
-        },
-        {
-          title: 'Bad Idea',
-          videos: [
-            { title: 'La strage di Erba', file: 'BadIdea_01 Scena Strage di Erba.mov' },
-            { title: "L'omicidio di Laura Ziliani", file: 'BadIdea_02 Scena Omicidio Laura Ziliani.mp4' }
-          ]
-        },
+    {
+      title: 'Reply Bitmama',
+      videos: [
+        { title: 'La strage di Erba', file: 'Reply_Scena_Strage_Di_Erba.mov' }
+      ]
+    },
+    {
+      title: 'Bad Idea',
+      videos: [
+        { title: 'La strage di Erba', file: 'BadIdea_01 Scena Strage di Erba.mov' },
+        { title: "L'omicidio di Laura Ziliani", file: 'BadIdea_02 Scena Omicidio Laura Ziliani.mp4' }
       ]
     }
   ]
 };
-
